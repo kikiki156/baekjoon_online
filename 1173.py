216309a6d6c6afc43f19bf2a2ecacd00c1054198
hanseongmin <5555565@naver.com>
2023-11-12 15:31:58 +0900

    1173 upload

--- 1173.py
+++ 1173.py
@@ -1,13 +1,17 @@
-N,m,M,T,R = map(int,input().split())
-resttime = 0
-practime = 0
-while 1:
-    if practime == N:
-        print(resttime+practime)
-        break
-    elif m+T<M:
-        practime+=1
-        m += T
-    elif m+T>M:
-        resttime+=1
-        m -= R
+N, m, M, T, R = map(int,input().split())
+now = m
+time = 0
+if M-m < T:
+    time = -1
+else:
+    while N > 0:
+        if now+T <= M:
+            now= now+T
+            N -= 1
+        else :
+            if now - R < m:
+                now = m
+            else:
+                now = now - R
+        time+=1
+print(time)
